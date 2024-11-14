2b50986dd567b3e2aadb0823a385070db3100de7
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-11-14 19:38:09 -0300

    feat: adicionado caso de uso para busca de academias proximas

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,8 @@
-import { GymsRepository } from '@/repositories/gyms-repository'
+import {
+  FindManyNearbyParams,
+  GymsRepository,
+} from '@/repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { Gym, Prisma } from '@prisma/client'
 import { randomUUID } from 'crypto'
 
@@ -31,5 +35,22 @@ export class InMemoryGymsRepository implements GymsRepository {
     }
     this.items.push(gym)
     return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        {
+          latitude: params.latitude,
+          longitude: params.longitude,
+        },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
   }
 }
